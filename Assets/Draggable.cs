591c4f9c20d3dcf8cce48ab8388df833eef8da2f
Thomas Castiglione <castiglione@mac.com>
2021-03-01 17:50:16 +0800

    nicer dragging behaviour: either hold-down or click, drag from the offset you started at

--- Assets/Draggable.cs
+++ Assets/Draggable.cs
@@ -1,25 +1,75 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(Sprite)), RequireComponent(typeof(Collider2D))]
-public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class Draggable : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerClickHandler
 {
+    bool draggingByEvent;
+    Camera draggingCamera;
+    Vector2 dragOffset;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (!draggingByEvent)
+        {
+            if (draggingCamera == null)
+            {
+                GrabPointer(eventData.pressEventCamera, eventData.position);
+            }
+            else
+            {
+                ReleasePointer();
+            }
+        }
+    }
+
     public void OnBeginDrag(PointerEventData eventData)
     {
+        GrabPointer(eventData.pressEventCamera, eventData.position);
+        draggingCamera = null;
+        draggingByEvent = true;
     }
 
     public void OnDrag(PointerEventData eventData)
     {
-        var plane = new Plane(Vector3.forward, transform.position);
-        var ray = eventData.pressEventCamera.ScreenPointToRay(eventData.position);
-        
-        if (plane.Raycast(ray, out var distance))
-        {
-            transform.position = ray.origin + ray.direction * distance;
-        }
+        FollowPointer(eventData.pressEventCamera, eventData.position);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
+        ReleasePointer();
+    }
+
+    public void Update()
+    {
+        if (draggingCamera != null)
+        {
+            FollowPointer(draggingCamera, Pointer.current.position.ReadValue());
+        }
+    }
+
+    private void GrabPointer(Camera camera, Vector3 position)
+    {
+        var worldPosition = camera.ScreenToWorldPoint(position);
+        draggingCamera = camera;
+        dragOffset = transform.position - worldPosition;
+    }
+
+    private void FollowPointer(Camera camera, Vector3 position)
+    {
+        var plane = new Plane(Vector3.forward, transform.position);
+        var ray = camera.ScreenPointToRay(position + (Vector3)dragOffset);
+
+        if (plane.Raycast(ray, out var distance))
+        {
+            transform.position = ray.origin + ray.direction * distance + (Vector3)dragOffset;
+        }
+    }
+
+    private void ReleasePointer()
+    {
+        draggingCamera = null;
+        draggingByEvent = false;
     }
 }
