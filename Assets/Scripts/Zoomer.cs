ac3bb8b9070d2b9ceec47896518e19309989c8a7
Thomas Castiglione <castiglione@mac.com>
2021-03-04 02:39:36 +0800

    implement panning by rightclick-drag

--- Assets/Scripts/Zoomer.cs
+++ Assets/Scripts/Zoomer.cs
@@ -1,24 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 // born 1995-2009
 public class Zoomer : MonoBehaviour
 {
-    public float speed = 500f;
-    Camera camera;
-    Controls controls;    
+    public float zoomSpeed = 10f;
 
-    public void Awake()
-    {
-        camera = GetComponent<Camera>();
-        controls = new Controls();
-        controls.UI.Zoom.performed += OnZoom;
-    }
+    private new Camera camera;
+    private Controls controls;
+    bool panning;
+    Vector2 lastScreenPosition;
 
     public void OnEnable()
     {
+        if (camera == null)
+        {
+            camera = GetComponent<Camera>();
+        }
+
+        if (controls == null)
+        {
+            controls = new Controls();
+            controls.UI.Zoom.performed += OnZoom;
+            controls.UI.Pan.performed += OnPan;
+            controls.UI.TriggerPan.started += OnBeginPan;
+            controls.UI.TriggerPan.canceled += OnEndPan;
+        }
+
         controls.UI.Enable();
     }
 
@@ -30,6 +38,33 @@ public class Zoomer : MonoBehaviour
     private void OnZoom(InputAction.CallbackContext context)
     {
         var value = context.ReadValue<Vector2>();
-        camera.orthographicSize = Mathf.Clamp(camera.orthographicSize - value.y / speed, 2f, 6f);
+        camera.orthographicSize = Mathf.Clamp(camera.orthographicSize - value.y * zoomSpeed, 2f, 6f);
+    }
+
+    private void OnBeginPan(InputAction.CallbackContext context)
+    {
+        panning = true;
+    }
+
+    private void OnEndPan(InputAction.CallbackContext context)
+    {
+        panning = false;
+    }
+
+    private void OnPan(InputAction.CallbackContext context)
+    {
+        var screenPosition = context.ReadValue<Vector2>();
+        
+        if (panning)
+        {
+            var worldPosition = camera.ScreenToWorldPoint(screenPosition);
+            var lastWorldPosition = camera.ScreenToWorldPoint(lastScreenPosition);
+
+            var worldDelta = lastWorldPosition - worldPosition;
+
+            camera.transform.position = camera.transform.position + worldDelta;
+        }
+
+        lastScreenPosition = screenPosition;
     }
 }
--- Assets/UI/Controls.cs
+++ Assets/UI/Controls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/UI/Controls.inputactions'
 
 using System;
 using System.Collections;
@@ -25,6 +25,22 @@ public class @Controls : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Analog"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""Pan"",
+                    ""type"": ""Value"",
+                    ""id"": ""7f0dc556-28a5-4bb8-b326-1c5ede8d47e7"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Trigger Pan"",
+                    ""type"": ""Button"",
+                    ""id"": ""ddab4864-0607-472b-b105-6c7acc470f9c"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -38,6 +54,28 @@ public class @Controls : IInputActionCollection, IDisposable
                     ""action"": ""Zoom"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""39ac0e42-ba17-409d-a142-ee9c9831bc12"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Pan"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c80ddf78-c3b3-419c-aed2-5566cfee1e2d"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Trigger Pan"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -47,6 +85,8 @@ public class @Controls : IInputActionCollection, IDisposable
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Zoom = m_UI.FindAction("Zoom", throwIfNotFound: true);
+        m_UI_Pan = m_UI.FindAction("Pan", throwIfNotFound: true);
+        m_UI_TriggerPan = m_UI.FindAction("Trigger Pan", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -97,11 +137,15 @@ public class @Controls : IInputActionCollection, IDisposable
     private readonly InputActionMap m_UI;
     private IUIActions m_UIActionsCallbackInterface;
     private readonly InputAction m_UI_Zoom;
+    private readonly InputAction m_UI_Pan;
+    private readonly InputAction m_UI_TriggerPan;
     public struct UIActions
     {
         private @Controls m_Wrapper;
         public UIActions(@Controls wrapper) { m_Wrapper = wrapper; }
         public InputAction @Zoom => m_Wrapper.m_UI_Zoom;
+        public InputAction @Pan => m_Wrapper.m_UI_Pan;
+        public InputAction @TriggerPan => m_Wrapper.m_UI_TriggerPan;
         public InputActionMap Get() { return m_Wrapper.m_UI; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -114,6 +158,12 @@ public class @Controls : IInputActionCollection, IDisposable
                 @Zoom.started -= m_Wrapper.m_UIActionsCallbackInterface.OnZoom;
                 @Zoom.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnZoom;
                 @Zoom.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnZoom;
+                @Pan.started -= m_Wrapper.m_UIActionsCallbackInterface.OnPan;
+                @Pan.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnPan;
+                @Pan.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnPan;
+                @TriggerPan.started -= m_Wrapper.m_UIActionsCallbackInterface.OnTriggerPan;
+                @TriggerPan.performed -= m_Wrapper.m_UIActionsCallbackInterface.OnTriggerPan;
+                @TriggerPan.canceled -= m_Wrapper.m_UIActionsCallbackInterface.OnTriggerPan;
             }
             m_Wrapper.m_UIActionsCallbackInterface = instance;
             if (instance != null)
@@ -121,6 +171,12 @@ public class @Controls : IInputActionCollection, IDisposable
                 @Zoom.started += instance.OnZoom;
                 @Zoom.performed += instance.OnZoom;
                 @Zoom.canceled += instance.OnZoom;
+                @Pan.started += instance.OnPan;
+                @Pan.performed += instance.OnPan;
+                @Pan.canceled += instance.OnPan;
+                @TriggerPan.started += instance.OnTriggerPan;
+                @TriggerPan.performed += instance.OnTriggerPan;
+                @TriggerPan.canceled += instance.OnTriggerPan;
             }
         }
     }
@@ -128,5 +184,7 @@ public class @Controls : IInputActionCollection, IDisposable
     public interface IUIActions
     {
         void OnZoom(InputAction.CallbackContext context);
+        void OnPan(InputAction.CallbackContext context);
+        void OnTriggerPan(InputAction.CallbackContext context);
     }
 }
