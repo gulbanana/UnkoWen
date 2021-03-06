7d69f5ffceeaac11b3754090bd94ebadb4f00398
Thomas Castiglione <castiglione@mac.com>
2021-03-07 01:22:45 +0800

    text choices with smooth scrolling

---
+++ Assets/Scripts/ChoiceLinks.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ChoiceLinks : MonoBehaviour, IPointerClickHandler
+{    
+    public TextMeshProUGUI mainText;
+    public TextMeshProUGUI choiceText;
+    public ScrollRect scrollView;
+    public float scrollSpeed = 1000f;
+    private float totalHeight;
+
+    private readonly string[] texts =
+    {
+        "\"Click here,\" I said, willing the player's cursor to my location.",
+        "\"...or here. That would also be an OK place to click.\" I knew the player wouldn't perceive my contempt."
+    };
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var linkIndex = TMP_TextUtilities.FindIntersectingLink(choiceText, eventData.position, eventData.pressEventCamera);
+        if (linkIndex != -1)
+        {
+            var link = choiceText.textInfo.linkInfo[linkIndex];
+
+            mainText.text += $"{texts[linkIndex]}\n\n";
+            totalHeight = mainText.GetComponentInParent<RectTransform>().rect.height;
+
+            StartCoroutine(SmoothScroll());
+        }
+    }
+
+    private IEnumerator SmoothScroll()
+    {
+        scrollView.verticalNormalizedPosition = Mathf.Clamp(scrollView.verticalNormalizedPosition, 0.0001f, 1);
+        while (scrollView.verticalNormalizedPosition > 0)
+        {
+            scrollView.verticalNormalizedPosition -= Time.deltaTime * scrollSpeed / totalHeight;
+            yield return null;
+        }
+    }
+}
