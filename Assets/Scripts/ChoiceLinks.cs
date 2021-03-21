40bacbd51496f9131b8c8fcf358379649287c008
Thomas Castiglione <castiglione@mac.com>
2021-03-21 23:24:49 +0800

    high contrast links

--- Assets/Scripts/ChoiceLinks.cs
+++ Assets/Scripts/ChoiceLinks.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(TextMeshProUGUI))]
+[ExecuteInEditMode]
 public class ChoiceLinks : MonoBehaviour
 {    
     private TextMeshProUGUI choiceText;
@@ -11,12 +12,20 @@ public class ChoiceLinks : MonoBehaviour
     public Color32 hoverColor;
     private int hoverLink = -1;
 
-    public void Start()
+    private void Start()
     {
         choiceText = GetComponent<TextMeshProUGUI>();
     }
 
-    public void LateUpdate()
+    private void OnValidate()
+    {
+        if (choiceText != null)
+        {
+            choiceText.color = baseColor;
+        }
+    }
+
+    private void LateUpdate()
     {
         var position = Mouse.current.position.ReadValue();
         var hovering = TMP_TextUtilities.IsIntersectingRectTransform(choiceText.rectTransform, position, hoverCam);
@@ -24,7 +33,7 @@ public class ChoiceLinks : MonoBehaviour
         if (hovering)
         {
             var linkIndex = TMP_TextUtilities.FindIntersectingLink(choiceText, position, hoverCam);
-            if (linkIndex != -1)
+            if (linkIndex != -1 && hoverLink != linkIndex)
             {
                 if (hoverLink != -1)
                 {
@@ -45,7 +54,7 @@ public class ChoiceLinks : MonoBehaviour
     private void HighlightLink(int linkIndex, Color32 color)
     {
         var linkInfo = choiceText.textInfo.linkInfo[linkIndex];
-
+        
         for (var i = 0; i < linkInfo.linkTextLength; i++)
         {
             var charIndex = linkInfo.linkTextfirstCharacterIndex + i;
@@ -53,7 +62,7 @@ public class ChoiceLinks : MonoBehaviour
             var meshIndex = charInfo.materialReferenceIndex;
             var vertexIndex = charInfo.vertexIndex;
             var vertexColors = choiceText.textInfo.meshInfo[meshIndex].colors32;
-
+            
             vertexColors[vertexIndex + 0] = color;
             vertexColors[vertexIndex + 1] = color;
             vertexColors[vertexIndex + 2] = color;
